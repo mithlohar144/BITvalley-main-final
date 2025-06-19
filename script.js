a31b1e0cbbb86f6421f82f8bab6219e497c7493d
Mithilesh Lohar <128236295+mithlohar144@users.noreply.github.com>
2025-06-19 16:44:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-script.js
+
+  const cursor = document.querySelector('.custom-cursor');
+
+  window.addEventListener('mousemove', (e) => {
+    gsap.to(cursor, {
+      x: e.clientX,
+      y: e.clientY,
+      duration: 0.5, // Smooth transition
+      ease: 'power2.out',
+    });
+  });
+
+  const links = document.querySelectorAll('a');
+
+  links.forEach((link) => {
+    link.addEventListener('mouseenter', () => {
+      gsap.to(cursor, { scale: 1, backgroundColor: '#070066', duration: 0.3 });
+    });
+
+    link.addEventListener('mouseleave', () => {
+      gsap.to(cursor, { scale: 2 ,zIndex:1,  backgroundColor: '#070066', duration: 0.1 });
+    });
+  });
+
+
+
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
